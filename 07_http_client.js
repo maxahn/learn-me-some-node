5c723d8af372ac62b0870d8eddf65f200c66b560
maxahn <ahnmaximus@gmail.com>
2016-07-04 19:07:22 -0700

    finished http client get request

--- 07_http_client.js
+++ 07_http_client.js
@@ -1,7 +1,11 @@
 var http = require('http');
 var getUrl = process.argv[2]; 
 http.get(getUrl, function callback (response) {
+  response.setEncoding('utf8');
   response.on('data', function (data) {
     console.log(data);
+  });
+  response.on('error', function(error) {
+    throw error; 
   });
 });
